2edeeae601da896d3ea49e0c96012198bfe613be
zt <sczhongtao@139.com>
2020-07-22 09:10:53 +0800

    '项目初始化'

---
+++ src/main/java/com/zt/springboot_mybatisplus_vue/SpringbootMybatisplusVueApplication.java
@@ -0,0 +1,13 @@
+package com.zt.springboot_mybatisplus_vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusVueApplication.class, args);
+    }
+
+}
